674e8f6a5626691dfc2e31af7f9f358c84dd5301
Bruno Garcia <brunoely.gc@gmail.com>
2024-11-04 17:48:39 -0300

    [rust-bitcoin] add `rust_bitcoin_des_block`

--- modules/rustbitcoin/rust_bitcoin_lib/src/lib.rs
+++ modules/rustbitcoin/rust_bitcoin_lib/src/lib.rs
@@ -1,9 +1,38 @@
 use std::slice;
 use std::os::raw::c_char;
-use std::ffi::{CStr, CString};
-use std::str::{FromStr, Utf8Error};
+use std::ffi::CString;
 
-use bitcoin::consensus::encode;
+use bitcoin::Block;
+use bitcoin::consensus::{deserialize_partial, encode};
+
+
+/// Creates a C string from a Rust str.
+///
+/// # Safety
+/// The caller must ensure that this memory is deallocated after the C string
+/// is no longer used. `into_raw` consumes the string and leaks the memory,
+/// making this allocation invisible to Rust's memory management.
+///
+/// This function panics if the input contains an internal null byte.
+unsafe fn str_to_c_string(input: &str) -> *mut c_char {
+    CString::new(input).unwrap().into_raw()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn rust_bitcoin_des_block(data: *const u8, len: usize) -> *mut c_char {
+    let data_slice = std::slice::from_raw_parts(data, len);
+    let res = deserialize_partial::<Block>(data_slice);
+
+    match res {
+        Ok(block) => str_to_c_string(&block.0.block_hash().to_string()),
+        Err(err) => {
+            if err.to_string().starts_with("unsupported segwit version") {
+                return str_to_c_string("unsupported segwit version")
+            }
+            str_to_c_string("")
+        },
+    }
+}
 
 #[no_mangle]
 pub unsafe extern "C" fn rust_bitcoin_script(data: *const u8, len: usize) -> bool {
