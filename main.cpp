9ecd366e46e6351d2ff37a41665bc48a2674c42a
Bruno Garcia <brunoely.gc@gmail.com>
2024-11-04 17:16:54 -0300

    [misc] remove example function

--- main.cpp
+++ main.cpp
@@ -10,15 +10,6 @@
 #include <modules/rustbitcoin/module.h>
 #endif
 
-bool FuzzMe(const uint8_t *Data, size_t DataSize)
-{
-  return DataSize >= 3 &&
-         Data[0] == 'F' &&
-         Data[1] == 'U' &&
-         Data[2] == 'Z' &&
-         Data[3] == 'Z'; // :‑<
-}
-
 std::shared_ptr<bitcoinfuzz::Driver> driver = nullptr;
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
