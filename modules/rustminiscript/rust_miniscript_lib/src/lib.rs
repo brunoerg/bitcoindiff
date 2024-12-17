e7801d70d7c0dd4530fb1392e1ab2f9f66f84b82
Bruno Garcia <brunoely.gc@gmail.com>
2024-12-17 10:36:25 -0300

    rust-miniscript: fix descriptor_parse target according to what Core does

--- modules/rustminiscript/rust_miniscript_lib/src/lib.rs
+++ modules/rustminiscript/rust_miniscript_lib/src/lib.rs
@@ -2,14 +2,12 @@ use std::os::raw::c_char;
 use std::ffi::CStr;
 use std::str::{FromStr, Utf8Error};
 
-use miniscript::DescriptorPublicKey;
+use miniscript::{policy, Miniscript, Segwitv0};
+use miniscript::policy::Liftable;
 
-/// Creates a Rust str from a C string.
-///
-/// # Safety
-/// The caller must ensure that the pointer points to a zero-terminated C string.
-/// If the pointer is null, this function will panic. A non-zero terminated string
-/// will trigger undefined behavior.
+type Script = Miniscript<String, Segwitv0>;
+type Policy = policy::Concrete<String>;
+
 unsafe fn c_str_to_str<'a>(input: *const c_char) -> Result<&'a str, Utf8Error> {
     CStr::from_ptr(input).to_str()
 }
@@ -20,8 +18,14 @@ pub unsafe extern "C" fn rust_miniscript_descriptor_parse(input: *const c_char) 
         return false;
     };
 
-    match DescriptorPublicKey::from_str(desc) {
+    match Policy::from_str(desc) {
         Err(_) => false,
-        Ok(_) => true,
+        Ok(pol) => {
+            // Use compile like in the fuzz target
+            match pol.compile::<Segwitv0>() {
+                Ok(_) => true,
+                Err(_) => false,
+            }
+        }
     }
 }
