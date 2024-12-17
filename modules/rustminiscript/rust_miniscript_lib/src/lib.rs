042bbe37e8f6b9715403531cc544d000958444a9
Bruno Garcia <brunoely.gc@gmail.com>
2024-12-17 10:40:39 -0300

    rust-miniscript: use `bitcoin::PublicKey` for parsing a descriptor

--- modules/rustminiscript/rust_miniscript_lib/src/lib.rs
+++ modules/rustminiscript/rust_miniscript_lib/src/lib.rs
@@ -6,7 +6,7 @@ use miniscript::{policy, Miniscript, Segwitv0};
 use miniscript::policy::Liftable;
 
 type Script = Miniscript<String, Segwitv0>;
-type Policy = policy::Concrete<String>;
+type Policy = policy::Concrete<miniscript::bitcoin::PublicKey>;
 
 unsafe fn c_str_to_str<'a>(input: *const c_char) -> Result<&'a str, Utf8Error> {
     CStr::from_ptr(input).to_str()
